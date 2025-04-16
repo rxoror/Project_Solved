9a78060c4e238e2e5d53a30bfc2f98fb96b6b40e
rxoror <hoyun767@gmail.com>
2025-04-16 04:53:57 +0000

    모든파일 업로드

---
+++ main.py
@@ -0,0 +1,7 @@
+###    UI 중심 실행 파일    ###
+import requests  # requests lib (API 요청)
+import pandas as pd  # pandas lib
+import json #json파일 처리 lib
+import streamlit as st # WEB UI 구현 lib 
+
+### test
