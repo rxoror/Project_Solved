5c66346eac404f62589bfd2ea2591be25a6f6eb8
rxoror <hoyun767@gmail.com>
2025-04-16 05:13:10 +0000

    .

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import requests  # requests lib (API 요청)
 import pandas as pd  # pandas lib
 import json #json파일 처리 lib
-import streamlit as st # WEB UI 구현 lib 
 
-### test
+import streamlit as st 
+## streamlit 라이브러리를 통해 추천시스템을 시각화 합니다 ##
+
+### main.py ###
